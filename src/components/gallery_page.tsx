cbb800fda5cd598ccfced0e003c481c129a45bb1
alexander-besch <mail@alexander-besch.com>
2026-01-05 20:20:34 +0100

    list of pictures

--- src/components/gallery_page.tsx
+++ src/components/gallery_page.tsx
@@ -12,13 +12,13 @@ export default function GalleryPage(): React.ReactNode {
 
             <div className="gallery_page_image_card">
                 <a href="/picture/fe986306-48f0-4f52-92ec-22461faa6b28">
-                    <Image inputPath="./resources/ID_7.jpg" lazy={false} />
+                    <Image inputPath="./resources/ID_7.jpg" lazy={false} transparent />
                 </a>
             </div>
 
             <div className="gallery_page_image_card">
                 <a href="/picture/a31f1c71-08c0-4332-bcad-29c0ae0b92af">
-                    <Image inputPath="./resources/Loewe.JPG" lazy={false} />
+                    <Image inputPath="./resources/Loewe.JPG" lazy={false} transparent />
                 </a>
             </div>
         </div>
