772047d8f2229438011f1e270e28beae27379324
alexander-besch <mail@alexander-besch.com>
2026-02-05 07:07:15 +0100

    404 page again

--- src/components/page_not_found_page.tsx
+++ src/components/page_not_found_page.tsx
@@ -6,13 +6,14 @@ export default function PageNotFoundPage(): React.ReactNode {
     return <Layout title="404 Page Not Found">
 
         <div className="page_not_found_page">
-            <h1>Page not found</h1>
+            <h1>404 Page not found</h1>
             <p>
-                This page is not the page you are looking for.
+                This page does not exist.<br />
                 If you followed a link, that link is broken.
                 <br />
-
-                Please send me a mail at <a href="mailto:mail@alexander-besch.com">mail@alexander-besch.com</a> and
+                <br />
+                Please send me a mail
+                at <a href="mailto:mail@alexander-besch.com">mail@alexander-besch.com</a> and
                 tell me where the broken link is.
                 <br /><br />
                 In any case you can always get back <a href="/">Home</a>.
